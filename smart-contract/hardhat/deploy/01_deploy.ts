4db85c54b596dc1ab037cc526e5b530469850b0d
alerex.eth <alessandromaci96@gmail.com>
2023-10-18 18:59:56 +0300

    splitting frontend and backend folder

--- smart-contract/hardhat/deploy/01_deploy.ts
+++ smart-contract/hardhat/deploy/01_deploy.ts
@@ -1,7 +1,5 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { parseEther } from "ethers";
-import { ethers } from "hardhat";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
