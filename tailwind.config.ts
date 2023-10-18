7eea74188c411cf441133fcc0dd5de942afaeca1
Mihir Sahu <114948140+0xmihirsahu@users.noreply.github.com>
2023-10-18 20:14:40 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,11 @@
 import type { Config } from 'tailwindcss'
-const {nextui} = require("@nextui-org/react");
+const { blackA } = require('@radix-ui/colors');
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './src/pages/*/.{js,ts,jsx,tsx,mdx}',
+    './src/components/*/.{js,ts,jsx,tsx,mdx}',
+    './src/app/*/.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -15,9 +14,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        ...blackA,
+      },
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 }
 export default config
