eacef8c2d66e844b7b33206cea19840f7e06eeb5
Mihir Sahu <114948140+0xmihirsahu@users.noreply.github.com>
2023-10-20 22:31:38 +0530

    design fix

--- next.js/src/app/components/TestTransaction.tsx
+++ next.js/src/app/components/TestTransaction.tsx
@@ -114,7 +114,6 @@ const TestTransaction: React.FC<{
   }, [isSuccess, isSuccessNative]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center min-w-min justify-between p-24">
       <div className="max-w-fit min-w-min p-4 rounded-xl items-center justify-between border-2 font-noto border-gray-400 text-sm lg:flex">
         <div className="w-full min-w-min">
           <button
@@ -141,7 +140,7 @@ const TestTransaction: React.FC<{
           </p>
           <br />
           <button
-            className="text-lg font-bold rounded-md p-2 text-gray-00 w-[150px] bg-sky-500 mb-8"
+            className="text-lg font-bold rounded-md p-2 text-gray-00 w-[150px] hover:bg-sky-400 bg-sky-500 mb-8"
             onClick={() => window.open("https://goerlifaucet.com/", "_blank")}
           >
             {"Get Goerli ETH"}
@@ -151,7 +150,7 @@ const TestTransaction: React.FC<{
             {"Test transaction details"}
           </h1>
           <div className="flex flex-row min-w-min h-fit mb-4 gap-2">
-            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-200 py-0 px-2">
+            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-100 py-0 px-2">
               <p className="font-semibold text-gray-950">{"Wallet:"}</p> &nbsp;
               <p className="text-[14.5px] font-semibold tracking-tighter">
                 {transactionData?.recipient.slice(0, 4)}
@@ -159,13 +158,13 @@ const TestTransaction: React.FC<{
                 {transactionData?.recipient.slice(-4)}
               </p>
             </div>
-            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-200 py-0 px-2">
+            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-100 py-0 px-2">
               <p className="font-semibold text-gray-950">{"Asset:"}</p> &nbsp;
               <p className="text-[14.5px] font-semibold tracking-tighter">
                 {"0.00169 gETH"}
               </p>
             </div>
-            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-200 py-0 px-2">
+            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-100 py-0 px-2">
               <p className="font-semibold text-gray-950">{"Network:"}</p> &nbsp;
               <p className="text-[14.5px] font-semibold tracking-tighter">
                 {"Goerli"}
@@ -177,7 +176,7 @@ const TestTransaction: React.FC<{
             {"Full transaction details"}
           </h1>
           <div className="flex flex-row min-w-min h-fit mb-12 gap-2">
-            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-200 py-0 px-2">
+            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-100 py-0 px-2">
               <p className="font-semibold text-gray-950">{"Wallet:"}</p> &nbsp;
               <p className="text-[14.5px] font-semibold tracking-tighter">
                 {transactionData?.recipient.slice(0, 4)}
@@ -185,13 +184,13 @@ const TestTransaction: React.FC<{
                 {transactionData?.recipient.slice(-4)}
               </p>
             </div>
-            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-200 py-0 px-2">
+            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-100 py-0 px-2">
               <p className="font-semibold text-gray-950">{"Asset:"}</p> &nbsp;
               <p className="text-[14.5px] font-semibold tracking-tighter">
                 {`${transactionData?.amount} ${transactionData?.token}`}
               </p>
             </div>
-            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-200 py-0 px-2">
+            <div className="border border-black h-fit flex flex-row rounded-2xl bg-gray-100 py-0 px-2">
               <p className="font-semibold text-gray-950">{"Network:"}</p> &nbsp;
               <p className="text-[14.5px] font-semibold tracking-tighter">
                 {transactionData?.chain}
@@ -199,7 +198,7 @@ const TestTransaction: React.FC<{
             </div>
           </div>
           <button
-            className="text-lg font-semibold rounded-md p-2 min-w-min text-gray-00 w-full bg-sky-500 mb-1"
+            className="text-lg font-semibold rounded-lg p-2 min-w-min text-gray-00 w-full hover:bg-sky-400 bg-sky-500 mb-1"
             type="button"
             onClick={() => sendTransactionTest?.()}
           >
@@ -207,7 +206,7 @@ const TestTransaction: React.FC<{
           </button>
           {transactionData?.securityLevel == "enhanced" ? (
             <button
-              className="text-lg font-semibold rounded-md border-2 min-w-min border-sky-500 p-2 bg-gray-00 w-full text-sky-500"
+              className="text-lg font-semibold rounded-lg border-2 min-w-min border-sky-500 p-2 bg-gray-00 w-full text-sky-500"
               type="button"
               onClick={onAnalysis}
             >
@@ -215,7 +214,7 @@ const TestTransaction: React.FC<{
             </button>
           ) : (
             <button
-              className="text-lg font-semibold rounded-md border-2 min-w-min border-sky-500 p-2 bg-gray-00 w-full text-sky-500"
+              className="text-lg font-semibold rounded-lg border-2 min-w-min border-sky-500 p-2 bg-gray-00 w-full text-sky-500"
               type="button"
               onClick={
                 transactionData?.isNativeTx
@@ -240,7 +239,6 @@ const TestTransaction: React.FC<{
           )}
         </div>
       </div>
-    </main>
   );
 };
 
