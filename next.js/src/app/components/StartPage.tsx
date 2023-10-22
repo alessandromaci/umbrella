92587881132836cd839825e80dd1333447ea1868
Mihir Sahu <0xmihirsahu@gmail.com>
2023-10-22 16:03:37 +0530

    subtle css fix

--- next.js/src/app/components/StartPage.tsx
+++ next.js/src/app/components/StartPage.tsx
@@ -214,8 +214,8 @@ const StartPage: React.FC<{
         >
           <Tabs.Root className="flex flex-col w-full" defaultValue="newAddress">
             <Tabs.List
-              className="shrink-0 flex border-b border-mauve6"
-              aria-label="Manage your account"
+              className="shrink-0 flex border-b border-gray-400"
+              aria-label="Enter Address"
             >
               <Tabs.Trigger
                 className="bg-zinc-900 text-gray-400 px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none select-none first:rounded-tl-md last:rounded-tr-md hover:text-sky-500 data-[state=active]:text-white data-[state=active]:shadow-sky-500 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:focus:relative outline-none cursor-default"
